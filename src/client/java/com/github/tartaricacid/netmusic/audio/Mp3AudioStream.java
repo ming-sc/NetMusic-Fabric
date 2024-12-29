b77c530b42359392e561a01ba583de7d90a2990f
IMG <1973581872@qq.com>
2024-12-29 17:46:34 +0800

    fix(audio): 修复音频流播放时断网无限期阻塞的问题

--- src/client/java/com/github/tartaricacid/netmusic/audio/Mp3AudioStream.java
+++ src/client/java/com/github/tartaricacid/netmusic/audio/Mp3AudioStream.java
@@ -3,7 +3,6 @@ package com.github.tartaricacid.netmusic.audio;
 import com.github.tartaricacid.netmusic.config.GeneralConfig;
 import javazoom.spi.mpeg.sampled.file.MpegAudioFileReader;
 import net.minecraft.client.sound.AudioStream;
-import org.apache.commons.compress.utils.IOUtils;
 import org.lwjgl.BufferUtils;
 
 import javax.sound.sampled.AudioFormat;
@@ -11,8 +10,11 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.ByteBuffer;
+import java.util.concurrent.*;
 
 /**
  * @author : IMG
@@ -22,9 +24,16 @@ public class Mp3AudioStream implements AudioStream {
     private final AudioInputStream stream;
     private final int frameSize;
     private final byte[] frame;
+    private boolean isPlaying;
 
     public Mp3AudioStream(URL url) throws UnsupportedAudioFileException, IOException {
-        AudioInputStream originalInputStream = new MpegAudioFileReader().getAudioInputStream(url);
+        URLConnection connection = url.openConnection();
+        // 设置连接超时时间和读取超时时间
+        connection.setConnectTimeout(500);
+        connection.setReadTimeout(500);
+        InputStream inputStream = connection.getInputStream();
+
+        AudioInputStream originalInputStream = new MpegAudioFileReader().getAudioInputStream(inputStream);
         AudioFormat originalFormat = originalInputStream.getFormat();
         AudioFormat targetFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, originalFormat.getSampleRate(), 16,
                 originalFormat.getChannels(), originalFormat.getChannels() * 2, originalFormat.getSampleRate(), false);
@@ -37,6 +46,7 @@ public class Mp3AudioStream implements AudioStream {
             this.stream = targetInputStream;
         }
         this.frameSize = stream.getFormat().getFrameSize();
+        this.isPlaying = true;
         frame = new byte[frameSize];
     }
 
@@ -58,16 +68,38 @@ public class Mp3AudioStream implements AudioStream {
     public ByteBuffer getBuffer(int size) throws IOException {
         // 创建指定大小的ByteBuffer
         ByteBuffer byteBuffer = BufferUtils.createByteBuffer(size);
-        int bytesRead = 0, count = 0;
-        // 循环读取数据直到达到指定大小或输入流结束
-        do {
-            // 读取下一部分数据
-            count = this.stream.read(frame);
-            // 将读取的数据写入ByteBuffer
-            if (count != -1) {
-                byteBuffer.put(frame);
+        if (isPlaying) {
+            CompletableFuture<Object> future = new CompletableFuture<>().completeAsync(() -> {
+                try {
+                    // 循环读取数据直到达到指定大小或输入流结束
+                    int bytesRead = 0, count = 0;
+                    do {
+                        // 读取下一部分数据
+                        count = this.stream.read(frame);
+                        // 将读取的数据写入ByteBuffer
+                        if (count != -1) {
+                            byteBuffer.put(frame);
+                        }
+                    } while (count != -1 && (bytesRead += frameSize) < size);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                return null;
+            });
+
+            try {
+                future.get(500, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            } catch (TimeoutException e) {
+                future.cancel(true);
+                this.isPlaying = false;
+                e.printStackTrace();
             }
-        } while (count != -1 && (bytesRead += frameSize) < size);
+        }
+
         // 翻转ByteBuffer，准备进行读取操作
         byteBuffer.flip();
         // 返回包含读取数据的ByteBuffer
@@ -77,5 +109,6 @@ public class Mp3AudioStream implements AudioStream {
     @Override
     public void close() throws IOException {
         stream.close();
+        this.isPlaying = false;
     }
 }
